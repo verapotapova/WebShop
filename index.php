42ef8dc77b7a0d99eb4e6dceacb86a389fbe8250
verapotapova <vera_potapova_95@mail.ru>
2015-12-20 15:59:28 +0400

    Update index.php

--- index.php
+++ index.php
@@ -8,14 +8,9 @@ $categoryItems = getCategory();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-	<title>Free CSS template by ChocoTemplates.com</title>
+	<title>ShopAround</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
 	<!--[if lte IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
-	
-	<meta name="keywwords" content="Shop Around - Great free html template for on-line shop. Use it as a start point for your on line business. The template can be easily implemented in many open source E-commerce platforms" />
-	<meta name="description" content="Shop Around - Great free html template for on-line shop. Use it as a start point for your on line business. The template can be easily implemented in many open source E-commerce platforms" />
-	
-	
 </head>
 <body>
 	
