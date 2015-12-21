1ad11e65dc3dff1387f390856015f6e6dca8d4c0
Vera <vera_potapova_95@mail.ru>
2015-12-21 17:01:36 +0400

    Vera

--- FeedBack.php
+++ FeedBack.php
@@ -9,9 +9,9 @@
      </head>
      <body>
      	
-     	<form action="PHPMailer.php" name="FeedBack" method="post">
+     	<form action="?type=10" name="FeedBack" method="post">
      		
-     		<textarea name="message" rows="7" cols="40" placeholder="сообщение"><?=$_POST ["message"]?></textarea>
+     		<textarea name="message" rows="7" cols="40" placeholder="сообщение"></textarea>
      		<br />
      		<button type="submit" name="send">Отправить</button>
      	</form>
--- PHPMailer.php
+++ PHPMailer.php
@@ -1,25 +1,21 @@
-
-<html>
-     
+          <meta charset="UTF-8"  >
           <?php 
-               require_once 'PHPMailer-master/PHPMailerAutoload.php';
-                 if (isset ($_POST ["send"])) {
+               require 'PHPMailer-master/PHPMailerAutoload.php';
+                 if (isset ($_POST["send"])) {
                         $mail = new PHPMailer;
-                      
-                        $message = $_POST ["message"];
                         // $mail->SMTPDebug = 2;
                         $mail->isSMTP();
-                        $mail->Host = 'smtp.mail.ru';
+                        $mail->Host = 'smtp.yandex.ru';
                         $mail->SMTPAuth = true;
-                        $mail->Username = 'vera_potapova_95@mail.com';
-                        $mail->Password = 'potapovavera1995';
+                        $mail->Username = 'verapotapova1995';
+                        $mail->Password = 'verapotapova';
                         $mail->SMTPSecure = 'SSL';
                         $mail->Port = '465';
                         // $mail->SMTPSecure = 'tls';
                         // $mail->Port = 587;
                         $mail->CharSet = 'UTF-8';
-                        $mail->setFrom('vera_potapova_95@mail.com', 'Mailer');
-                        $mail->addAddress('verapotapova95gmail.com');
+                        $mail->setFrom('verapotapova1995@yandex.ru', 'Mailer');
+                        $mail->addAddress('vera_potapova_95@mail.ru', 'Mailer');
                         $mail->Subject = 'Here is the subject';
                         $mail->Body = $_POST["message"];
                         $mail->isHTML(true);
